242b59d3280682c127881284e52ca3daa9418f54
Sushain <sushaind96@gmail.com>
2025-11-16 16:12:16 +0530

    Add repos

--- src/main/java/com/example/helloworld/modal/UserProfile.java
+++ src/main/java/com/example/helloworld/modal/UserProfile.java
@@ -3,6 +3,7 @@ package com.example.helloworld.modal;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "user_profiles")
 public class UserProfile {
 
     @Id
@@ -14,4 +15,55 @@ public class UserProfile {
     @OneToOne
     @JoinColumn(name="user_id")
     private User user;
+
+    public UserProfile() {
+    }
+
+    public UserProfile(Long id, String firstName, String lastName, String phoneNumber, User user) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
